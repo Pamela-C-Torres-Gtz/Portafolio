19056c85580497e2b36f2edf738a0d214b50a5b1
Pamela C. Torres Gutiérrez <pamelatgtz@gmail.com>
2019-10-29 09:36:26 -0600

    Actualizacion de js para que no envie nada vacio

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+/*creando el evento del boton de contactame*/
+
+document.getElementById("idBtnEnviar").addEventListener("click", function(){
+    let strNombre= document.getElementById("inputNombre").value;
+    let strCorreo= document.getElementById("inputEmail").value;
+    let strAsunto= document.getElementById("inputSobjet").value;
+    let strMensaje= document.getElementById("inputMensaje").value;
+
+    /*verificar que no se encuentren vacios los inputs*/
+    if(strNombre != "" && strCorreo != "" && strAsunto !="" && strMensaje !=""){
+        let datos ={
+            nombre: strNombre,
+            correo: strCorreo,
+            asunto: strAsunto,
+            mensaje: strMensaje,
+        };
+        newPromise(function(datos) 
+          .then(function(response, datos){
+              document.getElementById("inputNombre").value="";
+              document.getElementById("inputEmail").value="";
+              document.getElementById("inputSobjet").value="";
+              document.getElementById("inputMensaje").value="";
+          }).catch(function(error){
+              console.log(error);
+          });
+    }else {
+        alert("Porfavor revisa  todos los campos")
+    }
+});
+
